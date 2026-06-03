544ef8959cf7a6beeaaa42ff54c0d17e4a8d9d9b
Troy Lemons Jr <troylemonsjr@gmail.com>
2026-06-03 13:38:18 -0700

    cors improvement

--- server.js
+++ server.js
@@ -12,7 +12,17 @@ require("dotenv").config();
 /* ✅ Middleware (FIXED) */
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://troylemons.netlify.app"
+  ],
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type"],
+  credentials: false
+}));
+
+app.options("*", cors());
 
 /* Health check */
 app.get("/", (req, res) => {
